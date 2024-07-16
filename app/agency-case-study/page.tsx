1f9b404a6bde1c49dd3c3bd8a61e4007c21477c2
Ammaar Khan <ammaarkhan2021@gmail.com>
2024-07-16 16:18:15 +1000

    live view for web design agency case study

--- app/agency-case-study/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const agency = () => {
-  return (
-    <div>Soon</div>
-  )
-}
-
-export default agency
--- app/web-design-agency/page.tsx
+++ app/web-design-agency/page.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { GoArrowUpRight } from 'react-icons/go'
+
 
 const agency = () => {
   return (
-    <div className='flex justify-center items-center w-full h-screen'>In progress</div>
+    <div className='flex flex-col gap-4 justify-center items-center w-full h-screen'>
+      <span className='text-gray-400 uppercase font-semibold text-[12px] leading-[12px] tracking-widest'>
+        In progress
+      </span>
+
+      <a 
+      href='https://indx-agency.webflow.io/'
+      className='flex items-center text-gray-600 font-medium text-[12px] leading-[16px] my-2 mx-4'
+      target='_blank'>
+          Live view
+          <GoArrowUpRight className='ml-1'/>
+      </a>
+    </div>
   )
 }
 

85e55a1a4b3c48600313821a6ac733069b551290
Ammaar Khan <ammaarkhan2021@gmail.com>
2024-10-28 12:36:27 +1100

    debugging hero updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -64,7 +64,7 @@ const Hero = () => {
             className="flex items-center mt-4 gap-[16px] text-gray-600 font-normal text-[12px] text-center  rounded-full py-0.5 px-4 max-w-[480px]"
             variants={itemVariants(32, 0.4)} // y: 16, delay: 0
           >
-            I'm a 2nd year University student studying a double degree in Science & IT 
+            I&apos;m a 2nd year University student studying a double degree in Science & IT 
             with a passion for design and all things tech
           </motion.div>
 

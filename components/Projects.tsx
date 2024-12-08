bd2d3a32646f8d77b065aa4c761ebaf446d2aa46
Ammaar Khan <ammaarkhan2021@gmail.com>
2024-12-08 20:07:39 +1100

    Updated all projects and href links

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,8 +11,8 @@ const Projects = () => {
                     Here&apos;s some of my work:
                 </h1>
 
-                <a className='flex w-full'
-                href='web-design-agency'
+                <a className='flex w-full' /* refera trade */
+                href='refera-trade'
                 target="_blank" 
                 rel="noopener noreferrer">
                     <div className='project_card w-full mx-auto'>
@@ -29,6 +29,64 @@ const Projects = () => {
                             <div className='flex flex-col items-start'>
                                 <span className='text-gray-900 text-[16px] font-semibold leading-[20px]'>ReferaTrade</span>
                                 <span className='text-gray-500 text-[16px] font-semibold leading-[20px]'>Designing a design system</span>
+                            </div>
+                            <img 
+                                src="/images/add.svg" 
+                                alt="Add Icon" 
+                                className="w-[16px] h-[16px]" 
+                            />
+                        </div>
+
+                    </div>
+                </a>
+
+                <a className='flex w-full' /* hrms */
+                href='hrms-case-study'
+                target="_blank" 
+                rel="noopener noreferrer">
+                    <div className='project_card w-full mx-auto'>
+                        <span className='text-gray-500 text-[16px] font-semibold leading-[20px]'>
+                            2024</span>
+
+                            <img 
+                                src="/images/hrms_logo.svg" 
+                                alt="HRMS Logo" 
+                                className="w-[48px] h-[48px]" 
+                            />
+
+                        <div className='flex flex-col items-start gap-[16px]'>
+                            <div className='flex flex-col items-start'>
+                                <span className='text-gray-900 text-[16px] font-semibold leading-[20px]'>Personal Project</span>
+                                <span className='text-gray-500 text-[16px] font-semibold leading-[20px]'>Micro interactions study</span>
+                            </div>
+                            <img 
+                                src="/images/add.svg" 
+                                alt="Add Icon" 
+                                className="w-[16px] h-[16px]" 
+                            />
+                        </div>
+
+                    </div>
+                </a>
+
+                <a className='flex w-full' /* indx agency */
+                href='web-design-agency'
+                target="_blank" 
+                rel="noopener noreferrer">
+                    <div className='project_card w-full mx-auto'>
+                        <span className='text-gray-500 text-[16px] font-semibold leading-[20px]'>
+                            2023</span>
+
+                            <img 
+                                src="/images/indx_logo.svg" 
+                                alt="Indx Agency Logo" 
+                                className="w-[48px] h-[48px]" 
+                            />
+
+                        <div className='flex flex-col items-start gap-[16px]'>
+                            <div className='flex flex-col items-start'>
+                                <span className='text-gray-900 text-[16px] font-semibold leading-[20px]'>Indx Agency</span>
+                                <span className='text-gray-500 text-[16px] font-semibold leading-[20px]'>Landing page for a web design agency</span>
                             </div>
                             <img 
                                 src="/images/add.svg" 

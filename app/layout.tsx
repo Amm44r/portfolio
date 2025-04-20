aa222e26a4728e09a415418be4342f2f5644a207
Ammaar Khan <ammaarkhan2021@gmail.com>
2025-04-20 15:57:30 +1000

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,13 +36,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Instrument+Serif:ital@0;1&display=swap" 
         rel="stylesheet"
         />
-      <meta property="og:title" content="Ammaar's Portfolio" />
-      <meta property="og:description" content="Coded using NextJS" />
-      <meta property="og:image" content="https://ammaarkhan.netlify.app/images/og-image-preview.png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="628" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://ammaarkhan.netlify.app/" />
       </head>
       <body className={fontSans.className}>{children}
         

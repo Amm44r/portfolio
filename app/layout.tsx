0c1e96be2dccfe4e8cc75bc93e8740133fa76111
Ammaar Khan <ammaarkhan2021@gmail.com>
2025-04-20 15:59:26 +1000

    new again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";

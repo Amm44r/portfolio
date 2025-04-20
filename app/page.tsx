7559d8eb1c6a3cd8dcd93b78b02a224af0a481f8
Ammaar Khan <ammaarkhan2021@gmail.com>
2025-04-20 15:42:23 +1000

    add metadata to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import Head from "next/head";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
@@ -5,13 +8,30 @@ import Play from "@/components/Play";
 
 export default function Home() {
   return (
-    <main className="relative bg-gray-50 flex justify-center items-center flex-col overflow-hidden mx-auto ">
-      <div className="w-full">
-        <Hero />
-        <Projects />
-        <Play />
-        <Footer />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Ammaar's Portfolio</title>
+        <meta name="description" content="Coded using NextJS" />
+        <meta property="og:title" content="Ammaar's Portfolio" />
+        <meta property="og:description" content="Coded using NextJS" />
+        <meta
+          property="og:image"
+          content="https://ammaarkhan.netlify.app/images/og-image-preview.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="628" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ammaarkhan.netlify.app/" />
+      </Head>
+
+      <main className="relative bg-gray-50 flex justify-center items-center flex-col overflow-hidden mx-auto">
+        <div className="w-full">
+          <Hero />
+          <Projects />
+          <Play />
+          <Footer />
+        </div>
+      </main>
+    </>
   );
 }
